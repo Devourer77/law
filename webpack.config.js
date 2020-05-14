b319fd4101692d06b697284281f09d1b251b5ef6
devourer77 <devourer77@gmail.com>
2020-05-14 15:47:02 +0300

    upd

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+
+      {
+        test: /\.pug$/,
+        use: [
+          "html-loader",
+          "pug-html-loader"
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png|svg|webp)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: '',
+              name: '/[path][name].[ext]',
+            },
+          },
+        ]
+      },
+      {
+        test: /\.sass$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            // "style-loader", // style nodes from js strings
+            "css-loader",
+            "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.pug",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        include: /\/includes/,
+      }),
+    ],
+  },
+};
